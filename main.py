4c44608455b03eb37fc25d41692b8e4742344884
arodgom <albertorodriguez_16@hotmail.com>
2023-03-01 21:40:05 +0100

    actualizar scripts main y models info de Isabel

--- main.py
+++ main.py
@@ -4,52 +4,52 @@ import json
 import csv
 from models import Iris
 
-app = FastAPI() # primera app con fastapi
+app = FastAPI()
 
-MEDIA_ROOT = "iris.csv"
+MEDIA_ROOT = "./media/iris.csv"
 
 @app.get('/')
 async def test():
     return "Bienvenido a FastAPI"
 
 
-# Método GET a la URL "/iris/"
-# llamaremos a nuestra aplicación (app name + método permitido)
+# Método GET a la url "/iris/"
+# llamaremos a nuestra aplicación (<app name> + <método permitido>)
 @app.get('/iris/')
 async def iris(response:Response):
     try:
-        # Crear un dataframe con la información de Iris
+        # Crear un dataframe con la información de Iris:
         df = pd.read_csv(MEDIA_ROOT)
-        print(df)
-        # lo transformamos a json para poder getsionarlo desde el front
+        # print(df)
+        # lo transformamos a json para poder gestionarlo desde el front:
         data = df.to_json(orient="index")
-        # cargar la info con formato json:
+        # cargar la infromación con formato json:
         data = json.loads(data)
-        return data
-    
+        return data    
     except Exception as e:
         print("Error al cargar el csv %s" % str(e))
         response.status_code = status.HTTP_404_NOT_FOUND
-        return "404 NOT FOUND"
+        return "404 NOT FOUND"        
+
     
-# TODO: POST de insertar un nuevo dato en el csv
-# metodo post a la url "isertData/"
-@app.post("/insertData", status_code=201)
-async def insertData(item:Iris)
+# POST de insertar un nuevo dato en el csv, última línea
+# Método POST a la url "/insertData/"
+@app.post("/insertData/", status_code=201)
+async def insert(item:Iris):
     with open(MEDIA_ROOT, "a", newline="") as csvfile:
-        # nombres de los campos:
-        fieldnames = ['sepal_length' Sepal_Length: float
-Sepal_Width: float
-Setal_Length: float
-Petal_Width: float
-Species: str]
-        write = csv_DictWriter(csvfile, fieldnames=fieldnames)
-        #insertar los valores en la última fila
-        writer.writerow('sepal_length': item.sepal_length)
-        
+        # Nombres de los campos:
+        fieldnames = ['sepal_length','sepal_width',
+                      'petal_length','petal_width',
+                      'species']
+        # escribimos el csv:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        # insertar los valores en la ultima fila:
+        writer.writerow({'sepal_length': item.sepal_length, 
+                         'sepal_width': item.sepal_width,
+                         'petal_length': item.petal_length,
+                         'petal_width': item.petal_width,
+                         'species': item.species})
+        return item
 
-
-
-
-# TODO: PUT actualizar la última linea del csv
-# TODO: DELETE eliminar la últma linea del csv
+# TODO: PUT actualizar la última línea del csv
+# TODO: DELETE eliminar la última línea del csv
--- models.py
+++ models.py
@@ -1,9 +1,10 @@
+# Crearemos el modelo de los datos a aguardar
 from pydantic import BaseModel
 
 # Modelo de datos:
-class Iris(BaseModel)
-Sepal_Length: float
-Sepal_Width: float
-Setal_Length: float
-Petal_Width: float
-Species: str
+class Iris(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+    species: str
